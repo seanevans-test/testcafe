e2e8043df813c31b6009c2f3f83afeb0ef59f982
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-12-08 19:27:41 +0000

    added test for the Drag bar

--- tests/dragTest.js
+++ tests/dragTest.js
@@ -5,3 +5,9 @@ const slider = Selector('#slider');
 
 fixture ('Drag test')
 .page('https://devexpress.github.io/testcafe/example/');
+
+test('Drag Testcase', async t =>{
+    await t
+        .click(triedCheckbox)
+        .drag(slider, 360,0, {offsetX:10, offsetY:10});
+})
