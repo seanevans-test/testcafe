00ebd8f79fa6e041aa7785ce23aa50cd850f83a1
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-12-08 18:39:53 +0000

    added fixture and iframeTest, clicking on the text area of the iframe, deleting all and adding in a new message

--- tests/IframeTest.js
+++ tests/IframeTest.js
@@ -2,3 +2,14 @@ import{Selector} from 'testcafe';
 
 const iframeName = Selector('iframe#mce_0_ifr');
 const textArea = Selector('body#tinymce.mce-content-body');
+
+fixture("Iframe Fixture")
+.page("https://the-internet.herokuapp.com/iframe")
+
+test("iFrame test", async t =>{
+    await t
+        .switchToIframe(iframeName)
+        .click(textArea)
+        .pressKey('ctrl+a delete')
+        .typeText(textArea, 'Hello')
+})
