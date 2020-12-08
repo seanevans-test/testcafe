2c2cbe25cfd0768a83f726f02b5fd4fb7061088c
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-12-08 18:10:18 +0000

    updated tests for meta and page

--- tests/testMetaScript.js
+++ tests/testMetaScript.js
@@ -1,8 +1,18 @@
-fixture("First Fixture")
+fixture.meta('version', '1')("First Fixture")
     .page("https://devexpress.github.io/testcafe/");
 
-test.page("https://devexpress.github.io/testcafe/example/")
+test.meta('env', 'production')
+.page("https://devexpress.github.io/testcafe/example/")
 ("First Test", async t => {
+    await t 
+        .typeText("#developer-name", "sean")
+        .click("#macos")
+        .click("#submit-button");
+});
+
+test
+.page("https://devexpress.github.io/testcafe/example/")
+("Second Test", async t => {
     await t 
         .typeText("#developer-name", "sean")
         .click("#macos")
