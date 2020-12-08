fab63ffe52253b6f4eed883d023317f7d58d596d
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-12-08 18:59:59 +0000

    added in const/fixture for the uploadFile test

--- tests/UploadFile.js
+++ tests/UploadFile.js
@@ -1,2 +1,9 @@
 import {Selector} from 'testcafe';
 
+const fileUpload = Selector('input#file-upload');
+const uploadFileButton = Selector('input#file-submit.button');
+
+fixture("File Upload Fixture")
+    .page("https://the-internet.herokuapp.com/upload");
+
+    
