79c60d932bad3f1898d920686f086a5f90727e00
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-12-08 19:19:22 +0000

    added in test for PageLoadTimeout, setting it to 0 wait time

--- tests/setPageLoadTimeout.js
+++ tests/setPageLoadTimeout.js
@@ -1,2 +1,8 @@
 fixture("Set page timeout Fixture")
     .page("https://devexpress.github.io/testcafe/");
+
+    test("set page timeout Test", async t =>{
+        await t
+        .setPageLoadTimeout(0)
+        .navigateTo('http://devexpress.github.io/testcafe/');
+    });
