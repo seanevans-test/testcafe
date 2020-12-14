a9b6cf22364531d6ae125a866ae0ad5f28bbabab
seanevans-test <sean-.-evans@hotmail.co.uk>
2020-12-14 19:27:52 +0000

    used .skip command

--- tests/skipTest.js
+++ tests/skipTest.js
@@ -1,6 +1,14 @@
 fixture("First Fixture")
     .page("https://devexpress.github.io/testcafe/");
 
+test.skip.page("https://devexpress.github.io/testcafe/example/")
+("First Test", async t => {
+    await t 
+        .typeText("#developer-name", "sean")
+        .click("#macos")
+        .click("#submit-button");
+});
+
 test.page("https://devexpress.github.io/testcafe/example/")
 ("First Test", async t => {
     await t 
